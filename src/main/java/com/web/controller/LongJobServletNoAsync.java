53aa12e78568d7e577d829065a184c9e78472bec
dennesshen <dennesshen@gmail.com>
2021-07-02 00:42:25 +0800

    Java OCP

--- src/main/java/com/web/controller/LongJobServletNoAsync.java
+++ src/main/java/com/web/controller/LongJobServletNoAsync.java
@@ -8,26 +8,13 @@ package com.web.controller;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-/**
- *
- * @author dennesshen
- */
-class Delay implements Runnable {
-
-    @Override
-    public void run() {
-        try {
-            Thread.sleep(5000);
-        } catch (Exception e) {
-        }
-    }
-}
 
 @WebServlet("/controller/longjobNoAsync")
 public class LongJobServletNoAsync extends HttpServlet {
@@ -37,13 +24,20 @@ public class LongJobServletNoAsync extends HttpServlet {
         req.setCharacterEncoding("UTF-8");
         resp.setCharacterEncoding("UTF-8");
         resp.setContentType("text/html;charset=UTF-8");
-        Delay delay = new Delay();
-        Thread t = new Thread(delay);
-        for (int i = 0; i < 10; i++) {
-            resp.getWriter().print( (i+1)+"進行第"+(i+1)+"個工作"+"<p/>");
-            resp.getWriter().flush();
-            t.start();
-        }
-    }
+            for (int i = 0; i < 10; i++) {
 
+                try {
+                    resp.getWriter().print((i + 1) + "進行第" + (i + 1) + "個工作" + "<p/>");
+                    resp.getWriter().flush();
+                    Thread.sleep(1000);
+
+                } catch (IOException ex) {
+                    Logger.getLogger(LongJobServletNoAsync.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (InterruptedException ex) {
+                Logger.getLogger(LongJobServletNoAsync.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            }
+        
+
+    }
 }
